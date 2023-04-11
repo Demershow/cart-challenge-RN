fc1e7600866bcd9fdadbdc63f1c66eee85e19bbb
demerson <demersontorres520@gmail.com>
2023-04-10 22:43:00 -0300

    Aula #116 listando o carrinho de compras

--- src/screens/Cart/index.js
+++ src/screens/Cart/index.js
@@ -1,15 +1,36 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList, } from 'react-native'
+import { CartContext } from '../../Contexts/CartContext'
+import CardItem from '../../Components/CardItem'
 
 export default function Cart(){
+  const { cart, addItemCart, removeItemCart } = useContext(CartContext);
+
   return(
-    <View style={styles.container}>
-      <Text>Pagina Carrinho</Text>
+    <View style={stlyes.container}>
+      <FlatList
+        data={cart}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={ (item) => String(item.id) }
+        ListEmptyComponent={ () => <Text>Nenhum item no carrinho...</Text>}
+        renderItem={ ({ item }) => (
+          <CardItem
+            data={item}
+            addAmount={ () => addItemCart(item) }
+            removeAmount={ () => removeItemCart(item) }
+          />
+        )}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const stlyes = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#FAFAFA',
+    paddingStart: 14, 
+    paddingEnd: 14,
+    paddingTop: 14,
   }
 })
