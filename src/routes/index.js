3c03cdc557667897b01d9297f20260c117df2f05
demerson <demersontorres520@gmail.com>
2023-04-10 21:38:35 -0300

    Aula #113 começando a tela gome

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Home from '../screens/Home'
+import Cart from '../screens/Cart'
+
+const Stack = createNativeStackNavigator()
+
+export default function Routes() {
+  return(
+
+    <Stack.Navigator >
+    <Stack.Screen name='Home' component={Home} options={{
+      headerShown: false
+    }} />
+    <Stack.Screen name='Cart' component={Cart} options={{
+      headerTitle: "Meu Carrinho"
+    }} />
+  </Stack.Navigator>
+    )
+}
---
+++ src/screens/Cart/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, StyleSheet, Text } from 'react-native'
+
+export default function Cart() {
+ return (
+   <View style={styles.container} >
+    <Text>
+      salve
+    </Text>
+   </View >
+ );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
